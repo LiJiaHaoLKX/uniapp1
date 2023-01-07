3b3d8d99ed0bac65f2f45dcd9ef9fb2de35fe697
nnkj <449031074@qq.com>
2023-01-07 16:36:45 +0800

    完成search页面

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,24 +1,30 @@
 <template>
-	<view class="scroll-view-container">
-		<!-- 左侧的滑动区域 -->
-		<scroll-view scroll-y="true" class="left-scroll-view">
-			<view @click="changeActive(item)" v-for="(item,index) in cateList" class="left-scroll-view-item"
-				:class="{active:item.cat_id===active}">{{item.cat_name}}</view>
-		</scroll-view>
-		<!-- 右侧的滑动区域 -->
-		<scroll-view :scroll-top="scrollTop" scroll-y="true" class="right-scroll-view">
-			<view class="right-scroll-view-box" v-for="(item,index) in cateLevel2" :key="item.cat_id">
-				<view class="right-scroll-view-cateLevel2">
-					/ {{item.cat_name}} /
-				</view>
-				<view class="right-scroll-view-cateLevel3-box">
-					<view class="right-scroll-view-cateLevel3" v-for="(item2,index) in item.children" @click="goToGoodsList(item2)">
-						<image :src="item2.cat_icon"></image>
-						<p>{{item2.cat_name}}</p>
+	<view>
+		<!-- 自定义的组件 -->
+		<my-search @myclick="gotoSearch"></my-search>
+		<view class="scroll-view-container">
+
+			<!-- 左侧的滑动区域 -->
+			<scroll-view scroll-y="true" class="left-scroll-view">
+				<view @click="changeActive(item)" v-for="(item,index) in cateList" class="left-scroll-view-item"
+					:class="{active:item.cat_id===active}">{{item.cat_name}}</view>
+			</scroll-view>
+			<!-- 右侧的滑动区域 -->
+			<scroll-view :scroll-top="scrollTop" scroll-y="true" class="right-scroll-view">
+				<view class="right-scroll-view-box" v-for="(item,index) in cateLevel2" :key="item.cat_id">
+					<view class="right-scroll-view-cateLevel2">
+						/ {{item.cat_name}} /
+					</view>
+					<view class="right-scroll-view-cateLevel3-box">
+						<view class="right-scroll-view-cateLevel3" v-for="(item2,index) in item.children"
+							@click="goToGoodsList(item2)">
+							<image :src="item2.cat_icon"></image>
+							<p>{{item2.cat_name}}</p>
+						</view>
 					</view>
 				</view>
-			</view>
-		</scroll-view>
+			</scroll-view>
+		</view>
 	</view>
 </template>
 
@@ -34,7 +40,7 @@
 				//二级分类的列表
 				cateLevel2: [],
 				//距离顶部的距离
-				scrollTop:1
+				scrollTop: 1
 			}
 		},
 		onLoad() {
@@ -45,16 +51,19 @@
 		methods: {
 			async getCateList() {
 				//发送请求
-				const {data: res} = await uni.$http.get('/api/public/v1/categories')
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/categories')
 				if (res.meta.status !== 200) {
 					return uni.$showMsg()
 				}
-				 res.message.forEach((item)=>{
-					
-					item.children.forEach((item2)=>{
-						if(item2.children){
-							item2.children.forEach((item3)=>{
-								item3.cat_icon='https://api-ugo-web.itheima.net/'+item3.cat_icon.split('/')[3]+'/'+item3.cat_icon.split('/')[4]
+				res.message.forEach((item) => {
+					item.children.forEach((item2) => {
+						if (item2.children) {
+							item2.children.forEach((item3) => {
+								item3.cat_icon = 'https://api-ugo-web.itheima.net/' + item3
+									.cat_icon.split('/')[3] + '/' + item3.cat_icon.split('/')[
+										4]
 							})
 						}
 					})
@@ -63,18 +72,27 @@
 				this.cateLevel2 = this.cateList[0].children
 				uni.$showMsg('请求成功')
 			},
-			changeActive({cat_id}) {
+			changeActive({
+				cat_id
+			}) {
 				this.active = cat_id
-				this.cateList.forEach((item)=>{
-					if(item.cat_id === cat_id){
+				this.cateList.forEach((item) => {
+					if (item.cat_id === cat_id) {
 						this.cateLevel2 = item.children
 					}
 				})
-				this.scrollTop = this.scrollTop===0?1:0
+				this.scrollTop = this.scrollTop === 0 ? 1 : 0
 			},
-			goToGoodsList({cat_id}){
+			goToGoodsList({
+				cat_id
+			}) {
 				uni.navigateTo({
-					url:`/subpkg/goods_list/goods_list?cid=${cat_id}`
+					url: `/subpkg/goods_list/goods_list?cid=${cat_id}`
+				})
+			},
+			gotoSearch(){
+				uni.navigateTo({
+					url:'/subpkg/search/search'
 				})
 			}
 		}
@@ -116,28 +134,33 @@
 		.right-scroll-view {
 			height: 100vh;
 			width: 510rpx;
-			.right-scroll-view-box{
-				.right-scroll-view-cateLevel2{
+
+			.right-scroll-view-box {
+				.right-scroll-view-cateLevel2 {
 					text-align: center;
 					font-weight: bold;
 					margin-top: 30rpx;
 					font-size: 14px;
 				}
-				.right-scroll-view-cateLevel3-box{
+
+				.right-scroll-view-cateLevel3-box {
 					display: flex;
 					flex-direction: row;
 					flex-wrap: wrap;
 					align-content: space-around;
-					.right-scroll-view-cateLevel3{
+
+					.right-scroll-view-cateLevel3 {
 						width: 130rpx;
 						height: 130rpx;
 						margin-top: 30rpx;
 						padding: 20rpx;
-						image{
+
+						image {
 							width: 100%;
 							height: 80%;
 						}
-						p{
+
+						p {
 							text-align: center;
 							font-size: 14px;
 							font-weight: bold;
